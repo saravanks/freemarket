6bd16208d44000331f295a81a28add5f59910033
Andy March <youthclubrecords@gmail.com>
2018-10-12 02:26:10 -0700

    reorderStockTest

--- src/widgets/SelectList.js
+++ src/widgets/SelectList.js
@@ -3,9 +3,21 @@ export function SelectList(data,ListWidget){
   return class SelectList extends React.Component {
     render(){
       var _props = this.props
-      _props.value = ["one,two,three"]
+      const value = ["one,two,three"]
+      value.get=x=>value[x]
+      value.set=(x,y)=>value[x]=y
+      value.remove=x=>value.splice(x,1)
+      value.delete=x=>{
+        value.splice(x,1)
+        return value
+      }
+      value.insert=(x,y)=>{
+        value.splice(x,0,y)
+        return value
+      }
+      _props.value = value
       return(
-        <ListWidget {...this.props}/>
+        <ListWidget {..._props}/>
       )
     }
   }
