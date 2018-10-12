b604b8e1deaeae1ecd2e410dcb16359689ea1483
Andy March <youthclubrecords@gmail.com>
2018-10-12 01:52:46 -0700

    reorderStockTest

--- src/widgets/SelectList.js
+++ src/widgets/SelectList.js
@@ -2,6 +2,8 @@ import React from 'react'
 export function SelectList(data,ListWidget){
   return class SelectList extends React.Component {
     render(){
+      var _props = this.props
+      _props.value = ["one,two,three"]
       return(
         <ListWidget {...this.props}/>
       )
